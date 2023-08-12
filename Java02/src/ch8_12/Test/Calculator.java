f54a72c87141ac0aa29e1e84240ee4372e6cb43e
leivmox <leivmox@gmail.com>
2023-08-13 01:24:46 +0800

    upadate ideavimec

---
+++ Java02/src/ch8_12/Test/Calculator.java
@@ -0,0 +1,7 @@
+package ch8_12.Test;
+
+public class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
--- Java02/src/ch8_7/Test9/Circle.java
+++ Java02/src/ch8_7/Test9/Circle.java
@@ -8,7 +8,7 @@ public class Circle extends GeometricObject {
     public Circle() {
     }
 
-    public Circle(double radius) {
+    public Circle(double radius){
         this.radius = radius;
     }
 
