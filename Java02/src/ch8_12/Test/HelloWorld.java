3190651757770b4291a26e653428f33776396f48
leivmox <leivmox@gmail.com>
2023-09-10 22:58:02 +0800

    update

--- Java02/src/ch8_12/Test/HelloWorld.java
+++ Java02/src/ch8_12/Test/HelloWorld.java
@@ -1,7 +1,11 @@
 package ch8_12.Test;
 
 public class HelloWorld {
+    private int a = 1;
+    private String Tom = "汤姆";
+    private double b = 1.2;
+
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+
     }
 }
