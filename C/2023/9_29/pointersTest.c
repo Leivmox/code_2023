3e756df34498b4ee91a7671cdf46dd5953461cb3
leivmox <leivmox@gmail.com>
2023-10-03 23:23:38 +0800

    update

--- C/2023/9_29/pointersTest.c
+++ C/2023/9_29/pointersTest.c
@@ -6,19 +6,19 @@ int main()
     printf("%d\n", &arr[9]);
     printf("%d\n", &arr[0]);
     printf("======================\n");
-    int* a = &arr[9];
-    int* b = &arr[0];
-    
+    int *a = &arr[9];
+    int *b = &arr[0];
+
     printf("%d\n", a);
     printf("%d\n", b);
     printf("======================\n");
-    printf("%p\n",a-b);
-    printf("%p\n", &arr[9] - &arr[0]);
+    printf("%d\n", a - b);
+    printf("%d\n", &arr[9] - &arr[0]);
     printf("======================\n");
-int* p1 = 10000;
-int* p2 = 10004;
-int c = p2 - p1;
-printf("%d\n",c);
-        getchar();
+    // int* p1 = 10000;
+    // int* p2 = 10004;
+    // int c = p2 - p1;
+    // printf("%d\n",c);
+    getchar();
     return 0;
 }
